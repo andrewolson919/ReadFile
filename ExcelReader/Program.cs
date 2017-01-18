b0cfadf80a639110c1b76e440641a6c11e4d4123
andrew.olson919@gmail.com <andrew.olson919@gmail.com>
2017-01-17 18:50:07 -0700

    Saving File Path

--- ExcelReader/Program.cs
+++ ExcelReader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelReader
 {
@@ -10,8 +11,22 @@ namespace ExcelReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Did this Work?\n Oh Yeah, Hello World...");
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            string FilePath;
+            int rCount, cCount, row, col;
+
+            Console.WriteLine("Enter your file path:\n ");
+            FilePath = Console.ReadLine();
+
+            Console.WriteLine("Your file path is: '{0}' ", FilePath);
             Console.Read();
+
+            xlApp = new Excel.Application();
+
         }
     }
 }
