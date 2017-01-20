bd8fc306d7ad339ee50fa6ca105a7f3f6fbe1f73
SovietBear <trekkersforever@gmail.com>
2017-01-19 20:46:16 -0700

    Do you know why this fixes the IndexOutOfBounds?

--- ExcelReader/ExcelReader.cs
+++ ExcelReader/ExcelReader.cs
@@ -18,9 +18,9 @@ namespace ExcelReader
 
              var data = new string[rowCount, columnCount];
              
-             for (var rowNumber = 1; rowNumber <= rowCount; rowNumber++)
+             for (var rowNumber = 1; rowNumber < rowCount; rowNumber++)
              {
-                 for (var columnNumber = 1; columnNumber <= columnCount; columnNumber++)
+                 for (var columnNumber = 1; columnNumber < columnCount; columnNumber++)
                  {
                      var cell = range.Cells[rowNumber, columnNumber] as Excel.Range;
                      if (cell != null)
@@ -46,14 +46,15 @@ namespace ExcelReader
                var rows =result.GetLength(0);
                var columns=result.GetLength(1);
                
-               for(var rowNumber=1; rowNumber <= rows; rowNumber++)
+               for(var rowNumber=1; rowNumber < rows; rowNumber++)
                {
-                    for(var columnNumber=1; columnNumber <= columns; columnNumber++)
+                    for(var columnNumber=1; columnNumber < columns; columnNumber++)
                     {
                          Console.Write("{0}\t", result[rowNumber, columnNumber]);
                     }
                     Console.WriteLine();
                }
+              Console.Read();
           }
     }
 }
